834175331e695e61034c5f118c1bde1e2a86993c
sclo00ps <louginescott@gmail.com>
2021-06-15 22:33:10 -0400

    require stmts

---
+++ index.js
@@ -0,0 +1,7 @@
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+
+// Initialize a new Game object
+const Questions = [];
