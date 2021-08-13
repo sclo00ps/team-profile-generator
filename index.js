632f7f3fd9ed619f12ec1da3c7420ee175ec479b
sclo00ps <louginescott@gmail.com>
2021-08-12 20:55:29 -0700

    add start function, addMgr, addEngineer, addIntern functions

--- index.js
+++ index.js
@@ -11,106 +11,56 @@ const inquirer = require("inquirer");
 //const jest = require('jest');
 //file system required for writing to file
 const fs = require("fs");
-//const util = require('util');
+const util = require('util');
 
 //const writeFileAsync = util.promisify(fs.writeFile);
 
 // Initialize Questions object
 const teamMbrs = [];
 
-
-// create file to generate html
-//const generateHTML = (answers) =>
-//    `<!DOCTYPE html>
-//<html lang="en">
-//<head>
-// <meta charset="UTF-8">
-//  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-// <title>Document</title>
-//</head>
-//<body>
-// <div class="jumbotron jumbotron-fluid">
-// <div class="container">
-//   <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-//   <p class="lead">I am from ${answers.location}.</p>
-//   <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-//   <ul class="list-group">
-//     <li class="list-group-item">My GitHub username is ${answers.github}</li>
-//     <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-//   </ul>
-// </div>
-//</div>
-//</body>
-//</html>`;
-
 //start displays all actions user can select
-// const start = () => {
-//     return inquirer.prompt({
-//       name: 'action',
-//       type: 'list',
-//       message: 'What would you like to do?',
-//       choices: [
-//         'Add a new department',
-//         'Add a new employee role',
-//         'Add a new employee',
-//         'View all departments',
-//         'View all roles',
-//         'View all employees',
-//         'Update employee roles',
-//       ],
-//     })
-//     .then((answer) => {
-//       switch (answer.action) {
-//         case 'Add a new department':
-//           addDepartment();
-//           break;
+const start = () => {
+    return inquirer.prompt({
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'Add manager',
+        'Add engineer',
+        'Add intern',
+        'Exit',
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case 'Add manager':
+          addManager();
+          break;
 
-//         case 'Add a new employee role':
-//           addRole();
-//           break;
+          case 'Add engineer':
+            addEngineer();
+            break;
 
-//         case 'Add a new employee':
-//          addEmployee();
-//           break;
+        case 'Add intern':
+          addIntern();
+          break;
 
-//           case 'View all departments':
-//           viewDepartment();
-//           break;
+        case 'Exit':
+         addEmployee();
+          break;
+       
+        default:
+          console.log(`Invalid action: ${answer.action}`);
+          break;
+      }
+    });
+    };
 
-//         case 'View all roles':
-//           viewRole();
-//           break;
-
-//         case 'View all employees':
-//           viewEmployee();
-//           break;
-
-//         case 'Update employee roles':
-//           updateRole();
-//           break;
-
-//         default:
-//           console.log(`Invalid action: ${answer.action}`);
-//           break;
-//       }
-//     });
-//     };
-
+//add new manager
+const addManager = () => {
+    // prompt for department
 return inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'mgr',
-            message: 'Select the Manager value to begin entering your team manager information.',
-            choices: ["Manager"],
-            validate: (value) => {
-                if (value) {
-                    return true
-                } else { return 'a value must be entered to continue' }
-            }
-        },
-        {
+    .prompt({
             type: 'input',
             name: 'mgrname',
             message: 'What is the team manager name?',
@@ -149,59 +99,29 @@ return inquirer
                     return true
                 } else { return 'a value must be entered to continue' }
             }
-        },
-        {
-            type: 'list',
-            name: 'contchoices',
-            message: 'What do you want to do?',
-            choices: [
-                "Add a new team member",
-                "Exit"
-            ],
-            validate: (value) => {
-                if (value) {
-                    return true
-                } else { return 'a value must be entered to continue' }
-            }
-        },
-        //  }])
-
-        /*  .then((answers) => {
-          const htmlPageContent = generateHTML(answers);
-          if (answers.contchoices === "Exit") {
-          fs.writeFile('index.html', htmlPageContent, (err) =>
-              err ? console.log(err) : console.log('Successfully created index.html!')) 
-          } else {
-          return inquirer
-              .prompt([
-      {
-          type: 'list',
-          name: 'role',
-          message: 'What is the team member role?',
-          choices: ["Engineer", "Intern"],
-          validate: (value) => {
-              if (value) {
-                  return true
-              } else { return 'a value must be entered to continue' }
-          }
-      },
-      */
-
-        {
-            type: 'list',
-            name: 'addtlmbrs',
-            message: 'Please select the role of the additional team member ?',
-            choices: ["Engineer", "Intern"],
-            validate: (value) => {
-                if (value) {
-                    return true
-                } else { return 'a value must be entered to continue' }
-            }
-        },
-        {
+        })
+      
+        .then((answer) => {
+            // when finished prompting, insert a new role into the db 
+                  
+              (err) => {
+                if (err) throw err;
+                console.log('The new manager has been added successfully!');
+                // re-prompt the user for new action
+                start();
+              }
+        });
+          
+      };
+      
+    //add new engineer 
+    const addEngineer = () => {
+    // prompt for department
+    return inquirer
+            .prompt({
             type: 'input',
             name: 'engname',
-            message: 'What is the name of the engineer',
+            message: 'What is the name of the engineer?',
             validate: (value) => {
                 if (value) {
                     return true
@@ -237,34 +157,26 @@ return inquirer
                     return true
                 } else { return 'a value must be entered to continue' }
             }
-        },
-        {
-            type: 'list',
-            name: 'contchoices',
-            message: 'What do you want to do?',
-            choices: [
-                "Add a new team member",
-                "Exit"
-            ],
-            validate: (value) => {
-                if (value) {
-                    return true
-                } else { return 'a value must be entered to continue' }
-            }
-        },
-        {
-            type: 'list',
-            name: 'addtlmbrs',
-            message: 'Please select the role of the additional team member ?',
-            choices: ["Engineer", "Intern"],
-            validate: (value) => {
-                if (value) {
-                    return true
-                } else { return 'a value must be entered to continue' }
+        })
+        
+        .then((answer) => {
+            // when finished prompting, insert a new role into the db 
+                  
+              (err) => {
+                if (err) throw err;
+                console.log('The new engineer has been added successfully!');
+                // re-prompt the user for new action
+                start();
+              }
+        });
+          
+      };
 
-            }
-        },
-        {
+    //add new intern
+    const addIntern = () => {
+    // prompt for department
+    return inquirer
+    .prompt({
             type: 'input',
             name: 'intname',
             message: 'What is the name of the intern?',
@@ -313,111 +225,27 @@ return inquirer
                     return true
                 } else { return 'a value must be entered to continue' }
             }
-        },
-        {
-            type: 'list',
-            name: 'contchoices',
-            message: 'What do you want to do?',
-            choices: [
-                "Add a new team member",
-                "Exit"
-            ],
-            validate: (value) => {
-                if (value) {
-                    return true
-                } else { return 'a value must be entered to continue' }
-            }
-        },
-
-
-        //  }])
-
-        /*  .then((answers) => {
-          const htmlPageContent = generateHTML(answers);
-          if (answers.contchoices === "Exit") {
-          fs.writeFile('index.html', htmlPageContent, (err) =>
-              err ? console.log(err) : console.log('Successfully created index.html!')) 
-          } else {
-          return inquirer
-              .prompt([
-      {
-          type: 'list',
-          name: 'role',
-          message: 'What is the team member role?',
-          choices: ["Engineer", "Intern"],
-          validate: (value) => {
-              if (value) {
-                  return true
-              } else { return 'a value must be entered to continue' }
+        })
+        
+                
+    .then((answer) => {
+        // when finished prompting, insert a new role into the db 
+              
+          (err) => {
+            if (err) throw err;
+            console.log('The new intern has been added successfully!');
+            // re-prompt the user for new action
+            start();
+          }else {
+              return
           }
-      },
-      */
+    });
+      
+  };
 
-        //  ])
 
-        {
-            type: 'list',
-            name: 'addtlmbrs',
-            message: 'Please select the role of tthe additional team member ?',
-            choices: ["Engineer", "Intern"],
-            validate: (value) => {
-                if (value) {
-                    return true
-                } else { return 'a value must be entered to continue' }
-            }
-        },
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of the addtional team member?',
-            validate: (value) => {
-                if (value) {
-                    return true
-                } else { return 'a value must be entered to continue' }
-            }
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is the employee id of the additional team member?',
-            validate: (value) => {
-                if (value) {
-                    return true
-                } else { return 'a value must be entered to continue' }
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is the email address of the additional team member?',
-            validate: (value) => {
-                if (value) {
-                    return true
-                } else { return 'a value must be entered to continue' }
-            }
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: 'What is the name of the school of the additional team member?',
-            validate: (value) => {
-                if (value) {
-                    return true
-                } else { return 'a value must be entered to continue' }
-            }
-        },
-        {
-            // rethink add if stmt to exit chg validate stmt
-            type: 'list',
-            name: 'newmbr',
-            message: 'Would you like to add a new team member, if so select a role?',
-            choices: ["Manager", "Intern"],
-            validate: (value) => {
-                if (value) {
-                    return true
-                } else { return 'a value must be entered to continue' }
-            }
-        },
+
+       
     ])
     .then((answers) => {
         const htmlPageContent = generateHTML(answers);
